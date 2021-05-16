9c0c18b2b08ac3bbb3ca774cdbf4a3383c0502dd
iri6ka <ikonnova@gmail.com>
2021-05-16 20:16:55 +1000

    setting up config settings

---
+++ index.rb
@@ -0,0 +1,14 @@
+require 'dotenv'
+Dotenv.load
+
+#configuration settings
+client = ZendeskAPI::Client.new do |config|
+    # Mandatory (All info taken from zendesk api client github documentation):
+    config.url = "https://ikonnova.zendesk.com/api/v2/" # e.g. https://yoursubdomain.zendesk.com/api/v2
+    config.username = ENV['DB_USER']  # Basic / Token Authentication
+    config.password = ENV['DB_PASS']  # Choose one of the following depending on your authentication choice
+    config.retry = true  
+    config.raise_error_when_rate_limited = false
+  
+end
+
